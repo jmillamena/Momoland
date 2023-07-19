de671c46dcf3d7819bfeb163c3fe225e03a8d8c0
Jenna Millamena <jcmillamena@gmail.com>
2023-07-18 20:56:13 -1000

    adds main text

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const h2 = document.createElement("h2");
 
-h2.textContent = "This content is added by JavaScript";
+h2.textContent = "Momo in the Meadow";
 
 document.querySelector("body").appendChild(h2);
+
